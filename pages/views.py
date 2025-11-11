c501b767dc765f9eb2303a0f0ae18e97f63ff437
Esmeralda Camille <esmeraldacamille@gmail.com>
2025-11-11 17:42:18 -0500

    Esmeralda - 11/11 Updates

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def main_page(request):
+    return render(request, "main.html")
+
+@login_required
 def home_page(request):
     return render(request, "home.html")
