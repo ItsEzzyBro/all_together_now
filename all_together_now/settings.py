801798d188747caf2bb8ab47fd83effd74aad39f
A Z <msgiava77@gmail.com>
2025-11-30 09:26:51 -0500

    Google cloud switch changes

--- all_together_now/settings.py
+++ all_together_now/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 USE_SQLITEDB = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-@#5$o1k_jcoasj31cjvkanrix@x9dl)4vljtay@n-vee_t6=j)"
